047d3dc88c7f0426502dfad082911c54d432f3d4
QziuL <lf.quinholi2@gmail.com>
2025-04-14 21:04:15 -0300

    feat: call seeders

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(EixoSeeder::class);
+        $this->call(NivelSeeder::class);
+        $this->call(CursoSeeder::class);
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
     }
 }
